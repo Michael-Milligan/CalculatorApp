723c46a0e62153f06cc4a5b07179b99b33493ea9
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-07-05 23:00:54 +0300

    Made MainDesign

--- CalculatorApp/Calculator/MainWindow.xaml.cs
+++ CalculatorApp/Calculator/MainWindow.xaml.cs
@@ -24,5 +24,82 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Content = Convert.ToInt64(Field.Content) * 10 + 1;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Content = Convert.ToInt64(Field.Content) * 10 + 2;
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Content = Convert.ToInt64(Field.Content) * 10 + 3;
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Content = Convert.ToInt64(Field.Content) * 10 + 4;
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Content = Convert.ToInt64(Field.Content) * 10 + 5;
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Content = Convert.ToInt64(Field.Content) * 10 + 6;
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Content = Convert.ToInt64(Field.Content) * 10 + 7;
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Content = Convert.ToInt64(Field.Content) * 10 + 8;
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Content = Convert.ToInt64(Field.Content) * 10 + 9;
+        }
+
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+            Field.Content = Convert.ToInt64(Field.Content) * 10;
+        }
+
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMulti_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonSub_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
