d220121fc4a28071ef5d3895fe1fc37a0b95630b
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-07-22 22:54:23 +0300

    Made the MVVM
    But the app crashes when touching any button

--- CalculatorApp/Calculator/Model.cs
+++ CalculatorApp/Calculator/Model.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using Prism.Mvvm;
 
 namespace Calculator
@@ -13,6 +14,11 @@ namespace Calculator
         private double? Answer;
         private double Typed;
         private string Sign;
+
+        bool IsError;
+        double Memory;
+        int ZeroCount = 1;
+        bool IsAfterDot;
 
         public double? AnswerProperty { 
             get
@@ -57,7 +63,194 @@ namespace Calculator
             Application.Current.Shutdown();
         }
 
+        public void OppositeSign_Click(object sender, RoutedEventArgs e)
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            try
+            {
+                Window.Field.Content = Convert.ToDouble(Window.Field.Content) * (-1);
+            }
+            catch (Exception)
+            {
+                ErrorHandling();
+            }
+        }
+        public void Backspace_Click(object sender, RoutedEventArgs e)
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            if (Window.Answer.Content != null && Window.Sign.Content == null)
+            {
+                Window.Answer.Content = null;
+                return;
+            }
 
+            if (IsError)
+            {
+                ErrorHandling();
+                return;
+            }
+            //Transfer Content to the char array
+            char[] temp = (Window.Field.Content as string).ToCharArray();
+            //Copying all except for one to result
+            char[] Result = new char[temp.Length - 1];
+            for (int i = 0; i < Result.Length; ++i)
+            {
+                Result[i] = temp[i];
+            }
+            Window.Field.Content = Convert.ToString(Result);
+        }
+        public void Dot_Click(object sender, RoutedEventArgs e)
+        {
+            IsAfterDot = true;
+        }
+
+        public void C_Click(object sender, RoutedEventArgs e)
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            Window.Field.Content = 0;
+            Window.Answer.Content = null;
+            Window.Sign.Content = null;
+        }
+        public void CE_Click(object sender, RoutedEventArgs e)
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            Window.Field.Content = 0;
+        }
+
+        void GetError()
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            Window.Field.Content = "Error";
+            IsError = true;
+        }
+        void ErrorHandling()
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            Window.Field.Content = 0;
+            IsError = false;
+        }
+
+
+        public void Digit_Click(object sender, RoutedEventArgs e)
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            //Retrieving button from sender
+            Button button = sender as Button;
+
+            if (Window.Answer.Content != null && Window.Sign.Content == null)
+            {
+                Window.Answer.Content = null;
+                return;
+            }
+
+            //If the Field is overflown this will handle the infinity
+            if (Convert.ToString(Window.Field.Content) == "∞")
+            {
+                ErrorHandling();
+                return;
+            }
+
+            if (!IsAfterDot)
+            {
+                Window.Field.Content = Convert.ToDouble(Window.Field.Content) * 10 +
+                        (button.Name[6] - '0');
+            }
+            else
+            {
+                try
+                {
+                    //Rounding the double result to avoid "zero-trouble" (1*)
+                    Window.Field.Content = Math.Round(Convert.ToDouble(Window.Field.Content) +
+                            (button.Name[6] - '0') * Math.Pow(0.1, ZeroCount++), ZeroCount);
+                }
+                catch (Exception)
+                {
+                    GetError();
+                }
+            }
+        }
+
+        public void Action_Click(object sender, RoutedEventArgs e)
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            Button button = sender as Button;
+            if (Window.Answer.Content == null)
+            {
+                Window.Sign.Content = button.Tag;
+                Window.Answer.Content = Window.Field.Content;
+                Window.Field.Content = 0;
+            }
+            else if (Window.Answer.Content != null && Convert.ToDouble(Window.Field.Content) != 0)
+            {
+                Window.Answer.Content = GetResult(Convert.ToDouble(Window.Answer.Content), 
+                    Convert.ToDouble(Window.Field.Content));
+                Window.Field.Content = 0;
+                Window.Sign.Content = button.Tag;
+            }
+            else
+            {
+                Window.Sign.Content = button.Tag;
+            }
+        }
+
+
+        #region Memory
+        public void MPlus_Click(object sender, RoutedEventArgs e)
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            Memory += Convert.ToDouble(Window.Field.Content);
+        }
+
+        public void MMinus_Click(object sender, RoutedEventArgs e)
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            Memory -= Convert.ToDouble(Window.Field.Content);
+        }
+
+        public void MReset_Click(object sender, RoutedEventArgs e)
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            Window.Field.Content = Memory;
+        }
+        #endregion
+
+
+
+        public void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            Window.Answer.Content = GetResult(Convert.ToDouble(Window.Answer.Content),
+                Convert.ToDouble(Window.Field.Content));
+            Window.Field.Content = 0;
+            Window.Sign.Content = null;
+        }
+
+        double GetResult(double FirstOperand, double SecondOperand)
+        {
+            var Window = Application.Current.Windows[0] as MainWindow;
+            switch (Convert.ToString(Window.Sign.Content))
+            {
+                case "+":
+                    return FirstOperand + SecondOperand;
+                case "-":
+                    return FirstOperand - SecondOperand;
+                case "*":
+                    return FirstOperand * SecondOperand;
+                case "/":
+                    return FirstOperand / SecondOperand;
+                case "^":
+                    return Math.Pow(FirstOperand, SecondOperand);
+                case "y√x":
+                    try
+                    {
+                        return Math.Pow(FirstOperand, 1 / SecondOperand);
+                    }
+                    catch (Exception)
+                    { GetError(); }
+                    break;
+            }
+            return 0;
+        }
         #endregion
     }
 }
--- CalculatorApp/Calculator/VM.cs
+++ CalculatorApp/Calculator/VM.cs
@@ -19,6 +19,24 @@ namespace Calculator
 
         public DelegateCommand Exit_Click { get; }
 
+        public DelegateCommand C_Click { get; }
+        public DelegateCommand CE_Click { get; }
+
+        public DelegateCommand OppositeSign_Click { get; }
+        public DelegateCommand Backspace_Click { get; }
+        public DelegateCommand Dot_Click { get; }
+
+        public DelegateCommand Digit_Click { get; }
+
+        public DelegateCommand Action_Click { get; }
+
+        public DelegateCommand MPlus_Click { get; }
+        public DelegateCommand MMinus_Click { get; }
+        public DelegateCommand MReset_Click { get; }
+
+        public DelegateCommand Equals_Click { get; }
+
+
         public VM()
         {
             Model.PropertyChanged += (Sender, Args) =>
@@ -27,6 +45,57 @@ namespace Calculator
             Exit_Click = new DelegateCommand(() =>
             {
                 Model.Exit_Click(1, new RoutedEventArgs());
+            });
+
+            C_Click = new DelegateCommand(() =>
+                {
+                    Model.C_Click(1, new RoutedEventArgs());
+                });
+            CE_Click = new DelegateCommand(() =>
+                {
+                    Model.C_Click(1, new RoutedEventArgs());
+                });
+
+            Backspace_Click = new DelegateCommand(() =>
+            {
+                Model.Backspace_Click(1, new RoutedEventArgs());
+            });
+            Dot_Click = new DelegateCommand(() =>
+            {
+                Model.Dot_Click(1, new RoutedEventArgs());
+            });
+            OppositeSign_Click = new DelegateCommand(() =>
+            {
+                Model.OppositeSign_Click(1, new RoutedEventArgs());
+            });
+
+
+            Digit_Click = new DelegateCommand(() =>
+            {
+                Model.Digit_Click(1, new RoutedEventArgs());
+            });
+
+            Action_Click = new DelegateCommand(() =>
+            {
+                Model.Action_Click(1, new RoutedEventArgs());
+            });
+
+            MPlus_Click = new DelegateCommand(() =>
+            {
+                Model.MPlus_Click(1, new RoutedEventArgs());
+            }); 
+            MMinus_Click = new DelegateCommand(() =>
+            {
+                Model.MMinus_Click(1, new RoutedEventArgs());
+            }); 
+            MReset_Click = new DelegateCommand(() =>
+            {
+                Model.MReset_Click(1, new RoutedEventArgs());
+            });
+
+            Equals_Click = new DelegateCommand(() =>
+            {
+                Model.Equals_Click(1, new RoutedEventArgs());
             });
         }
     }
